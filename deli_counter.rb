a0536890aa0473eae88214c0ba5a3480baa4e79a
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-11 15:12:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
+  if katz_deli.length < 0
+  line = []
+  katz_deli.each_with_index do |customer, index|
+    line << puts "#{index + 1}. #{customer}"
   end
+  line
+  else
+  puts "The line is currently empty."
 end
